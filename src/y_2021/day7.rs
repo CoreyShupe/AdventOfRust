86b448e94892035a1d8603a04fb326d6470c2f47
CoreyShupe <coreyshupe101@gmail.com>
2021-12-07 02:43:48 -0500

    Rustfmt

--- src/y_2021/day7.rs
+++ src/y_2021/day7.rs
@@ -2,7 +2,9 @@ const INPUT: &str = include_str!("../../input/2021/day7.txt");
 
 fn solve<F: Fn(&[i64], i64) -> i64>(determine_fuel_usage: F) {
     let vec: Vec<i64> = INPUT.split(',').map(|x| x.parse().unwrap()).collect();
-    let minimum_usage = (0..*vec.iter().max().unwrap()).fold(i64::MAX, |acc, position| std::cmp::min(acc, determine_fuel_usage(&vec, position)));
+    let minimum_usage = (0..*vec.iter().max().unwrap()).fold(i64::MAX, |acc, position| {
+        std::cmp::min(acc, determine_fuel_usage(&vec, position))
+    });
     dbg!(minimum_usage);
 }
 
@@ -15,5 +17,9 @@ pub fn sol2() {
         ((num * num) + num) / 2
     }
 
-    solve(|vec, position| vec.iter().map(|x| addative_factorial((x - position).abs())).sum());
+    solve(|vec, position| {
+        vec.iter()
+            .map(|x| addative_factorial((x - position).abs()))
+            .sum()
+    });
 }
