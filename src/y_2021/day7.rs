1d16e1ea83669758b6468b40b2227a524ba3421c
CoreyShupe <coreyshupe101@gmail.com>
2021-12-07 02:43:27 -0500

    Cleanup

--- src/y_2021/day7.rs
+++ src/y_2021/day7.rs
@@ -1,42 +1,19 @@
 const INPUT: &str = include_str!("../../input/2021/day7.txt");
 
-pub fn parse_input() -> Vec<i64> {
-    INPUT.split(',').map(|x| x.parse().unwrap()).collect()
+fn solve<F: Fn(&[i64], i64) -> i64>(determine_fuel_usage: F) {
+    let vec: Vec<i64> = INPUT.split(',').map(|x| x.parse().unwrap()).collect();
+    let minimum_usage = (0..*vec.iter().max().unwrap()).fold(i64::MAX, |acc, position| std::cmp::min(acc, determine_fuel_usage(&vec, position)));
+    dbg!(minimum_usage);
 }
 
 pub fn sol1() {
-    fn determine_fuel_usage(vec: &[i64], position: i64) -> i64 {
-        vec.iter().map(|x| (x - position).abs()).sum()
-    }
-    
-    let vec = parse_input();
-    let max= vec.iter().max().unwrap();
-    let mut usage_map = std::collections::HashMap::new();
-    dbg!(max);
-    for position in 0..*max {
-        usage_map.insert(position, determine_fuel_usage(&vec, position));
-    }
-    dbg!(usage_map.iter().min_by_key(|x| x.1));
+    solve(|vec, position| vec.iter().map(|x| (x - position).abs()).sum());
 }
 
 pub fn sol2() {
     fn addative_factorial(num: i64) -> i64 {
-        if num == 0 {
-            return 0;
-        }
-        num + addative_factorial(num - 1)
+        ((num * num) + num) / 2
     }
 
-    fn determine_fuel_usage(vec: &[i64], position: i64) -> i64 {
-        vec.iter().map(|x| addative_factorial((x - position).abs())).sum()
-    }
-    
-    let vec = parse_input();
-    let max= vec.iter().max().unwrap();
-    let mut usage_map = std::collections::HashMap::new();
-    dbg!(max);
-    for position in 0..*max {
-        usage_map.insert(position, determine_fuel_usage(&vec, position));
-    }
-    dbg!(usage_map.iter().min_by_key(|x| x.1));
+    solve(|vec, position| vec.iter().map(|x| addative_factorial((x - position).abs())).sum());
 }
